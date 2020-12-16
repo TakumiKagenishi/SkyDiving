e178317e9e2dd42cca14d7516adf39dafec48444
Takumi-Kagenishi <rip.mymanzzz@gmail.com>
2020-12-16 12:48:31 +0900

    メインカメラをペンギンに追従させる

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject playerObj;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - playerObj.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerObj != null)
+        {
+            transform.position = playerObj.transform.position + offset;
+        }
+    }
+}
