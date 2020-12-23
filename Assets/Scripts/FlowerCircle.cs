f9ad3e585b556143a2d16e64cc16bb55c0cbd3b6
Takumi-Kagenishi <rip.mymanzzz@gmail.com>
2020-12-23 10:53:15 +0900

    花輪通過時の得点を更新し、画面に反映させる

---
+++ Assets/Scripts/FlowerCircle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class FlowerCircle : MonoBehaviour
+{
+    [Header("花輪通過時の得点")]
+    public int point;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.DORotate(new Vector3(0, 360, 0), 5.0f, RotateMode.FastBeyond360).SetEase(Ease.Linear).SetLoops(-1, LoopType.Restart);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -13,15 +15,20 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private float x;
     private float z;
+    private Vector3 straightRotation = new Vector3(180, 0, 0);
+    private int score;
     [SerializeField, Header("水しぶきのエフェクト")]
     private GameObject splashEffectPrefab = null;
     [SerializeField, Header("水しぶきのSE")]
     private AudioClip splashSE = null;
+    [SerializeField]
+    private Text txtScore;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        transform.eulerAngles = straightRotation;
     }
 
     // Update is called once per frame
@@ -44,7 +51,27 @@ public class PlayerController : MonoBehaviour
             effect.transform.position = new Vector3(effect.transform.position.x, effect.transform.position.y, effect.transform.position.z -0.5f);
             Destroy(effect, 2.0f);
             AudioSource.PlayClipAtPoint(splashSE, transform.position);
+            StartCoroutine(OutOfWater());
             //Debug.Log("着水 :" + inWater);
         }
+
+        if(col.gameObject.tag == "FlowerCircle")
+        {
+            //Debug.Log("花輪ゲット");
+            score += col.transform.parent.GetComponent<FlowerCircle>().point;
+            //Debug.Log("現在の得点 : " + score);
+            txtScore.text = score.ToString();
+        }
+    }
+    /// <summary>
+    /// 水面に顔を出す
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator OutOfWater()
+    {
+        yield return new WaitForSeconds(1.0f);
+        rb.isKinematic = true;
+        transform.eulerAngles = new Vector3(-30, 180, 0);
+        transform.DOMoveY(4.7f, 1.0f);
     }
 }
