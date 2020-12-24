02c8f65a48e730bf29608702ac0ec17061e2d96c
Takumi-Kagenishi <rip.mymanzzz@gmail.com>
2020-12-25 04:29:22 +0900

    キャラと水面の距離
    キャラと水面の距離の差分を計算する。その差分をConsoleビューに表示させる。その差分が0以下になったら計測を停止する。

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerController player;
+
+    [SerializeField]
+    private Transform goal;
+
+    private float distance;
+
+    private bool isGoal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGoal == true)
+        {
+            return;
+        }
+        
+        distance = player.transform.position.y - goal.transform.position.y;
+        Debug.Log(distance.ToString("F2"));
+
+        if (distance <= 0)
+        {
+            isGoal = true;
+        }
+    }
+}
