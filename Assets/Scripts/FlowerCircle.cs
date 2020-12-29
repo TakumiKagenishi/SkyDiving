def633ea22400deb2ed9bcb422474267329b09da
Takumi-Kagenishi <rip.mymanzzz@gmail.com>
2020-12-29 11:38:13 +0900

    花輪をくぐる際の演出
    花輪をくぐった時中心に集める演出。エフェクトを生成。

--- Assets/Scripts/FlowerCircle.cs
+++ Assets/Scripts/FlowerCircle.cs
@@ -7,11 +7,36 @@ public class FlowerCircle : MonoBehaviour
 {
     [Header("花輪通過時の得点")]
     public int point;
-    
+    [SerializeField]
+    private BoxCollider boxCollider;
+    [SerializeField]
+    private GameObject effectPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
         transform.DORotate(new Vector3(0, 360, 0), 5.0f, RotateMode.FastBeyond360).SetEase(Ease.Linear).SetLoops(-1, LoopType.Restart);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        boxCollider.enabled = false;
+        transform.SetParent(other.transform);
+        StartCoroutine(PlayGetEffect());
+    }
+    /// <summary>
+    /// 花輪をくぐった際の演出
+    /// </summary>
+    private IEnumerator PlayGetEffect()
+    {
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(transform.DOScale(Vector3.zero, 1.0f));
+        sequence.Join(transform.DOLocalMove(Vector3.zero, 1.0f));
+        yield return new WaitForSeconds(1.0f);
+        GameObject effect = Instantiate(effectPrefab, transform.position, Quaternion.identity);
+        effect.transform.position = new Vector3(effect.transform.position.x, effect.transform.position.y -1.5f, effect.transform.position.z);
+        Destroy(effect, 1.0f);
+        Destroy(gameObject, 1.0f);
     }
 
     // Update is called once per frame
