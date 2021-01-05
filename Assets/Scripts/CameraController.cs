65b4bd9d7db8333c4cb2b8b9f065b11261ca866d
Takumi-Kagenishi <rip.mymanzzz@gmail.com>
2021-01-05 13:03:01 +0900

    ボタンを押すたびにカメラを切り替える
    カメラ切り替えボタンを設置。それを押すたびにFPSCamera、SelfishCamera、MainCameraをループする。

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField]
     private PlayerController playerController;
+
+    [SerializeField]
+    private Camera fpsCamera;
+
+    [SerializeField]
+    private Camera selfishCamera;
+
+    [SerializeField]
+    private Button btnChangeCamera;
+
+    private int cameraIndex;
+
+    private Camera mainCamera;
+    
     private Vector3 offset;
+
     // Start is called before the first frame update
     void Start()
     {
         offset = transform.position - playerController.transform.position;
+        mainCamera = Camera.main;
+        btnChangeCamera.onClick.AddListener(ChangeCamera);
+        SetDefaultCamera();
     }
 
     // Update is called once per frame
@@ -25,5 +44,43 @@ public class CameraController : MonoBehaviour
         {
             transform.position = playerController.transform.position + offset;
         }
+    }
+
+    /// <summary>
+    /// カメラを変更(ボタンを押すたびに呼び出される)
+    /// </summary>
+    private void ChangeCamera()
+    {
+        switch(cameraIndex)
+        {
+            case 0:
+                cameraIndex++;
+                mainCamera.enabled = false;
+                fpsCamera.enabled = true;
+                break;
+
+            case 1:
+                cameraIndex++;
+                fpsCamera.enabled = false;
+                selfishCamera.enabled = true;
+                break;
+
+            case 2:
+                cameraIndex = 0;
+                selfishCamera.enabled = false;
+                mainCamera.enabled = true;
+                break;
+        }
+    }
+
+    /// <summary>
+    /// カメラを初期カメラ(三人称カメラ)に戻す
+    /// </summary>
+    public void SetDefaultCamera()
+    {
+        cameraIndex = 0;
+        mainCamera.enabled = true;
+        fpsCamera.enabled = false;
+        selfishCamera.enabled = false;
     }
 }
